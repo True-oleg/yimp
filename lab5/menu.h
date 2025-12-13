1a86425eeb0ce8573e90826355730671f9740c06
Ghostone <olgatroubina@gmail.com>
2025-12-13 03:04:06 +0300

    Create menu.h

---
+++ lab5/menu.h
@@ -0,0 +1,12 @@
+#ifndef MENU_H
+#define MENU_H
+#include "data.h"
+
+void print_menu(void);
+void display(GameArray* array);
+void search(GameArray* array);
+void add_game(GameArray* array);
+void delete_game(GameArray* array);
+void add_desc_game(GameArray* array);
+
+#endif
